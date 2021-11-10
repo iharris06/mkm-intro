04aa8eb9dd5dfde2a36e6720d4c9cf23f91202a9
iharris06 <ian.harris4@gmail.com>
2021-11-09 20:47:17 -0700

    Fix page to use image as background. Need to make responsive.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+import logo from './Michelle_banner.svg'
 import './App.css';
 import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-      <div class="Container">
-        <h1 className="title">Michelle Moyer</h1>  
-        <h2 className="title-2">Music</h2>  
-        <div className="social">
+      <div>
+        <div className="Container">
+          <h1 className="title">Michelle Moyer</h1>  
+          <h2 className="subtitle">Music</h2>  
+          <img src={logo} className="App-logo" alt="Michelle playing guitar" />
+          <div className="social-box">
           <SocialIcon className="social" bgColor="white" url="https://www.youtube.com/user/MichelleKMoyer"/>
           <SocialIcon className="social" bgColor="white" url="https://instagram.com/michellemoyermusic?utm_medium=copy_link"/>
           <SocialIcon className="social" bgColor="white" url="https://open.spotify.com/artist/4Y8GKCTKBhbwcl0nrSizwq?si=nAqW8Q0uTZ-ilQDgcadYTg"/>
           <SocialIcon className="social" bgColor="white" network="itunes" url="https://music.apple.com/us/artist/michelle-moyer/389392280"/>
-          {/* <SocialIcon className="social" bgColor="#f94c57" network="itunes" url="https://music.apple.com/us/artist/michelle-moyer/389392280"/> */}
         </div>
-      </div>
+        </div>
       </div>
     </div>
   );
