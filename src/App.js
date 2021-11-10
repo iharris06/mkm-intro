04735df2d2a619610df8d330ed2e0220393852f4
iharris06 <ian.harris4@gmail.com>
2021-11-09 21:28:19 -0700

    Make app responsive.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ function App() {
     <div className="App">
       <div>
         <div className="Container">
-          <h1 className="title">Michelle Moyer</h1>  
-          <h2 className="subtitle">Music</h2>  
-          <img src={logo} className="App-logo" alt="Michelle playing guitar" />
+          {/* Title */}
+          <div className="title-bar">
+            <h1 className="title">Michelle Moyer</h1>  
+            <h2 className="subtitle">Music</h2> 
+          </div>
+
+          {/* Image */}
+          <div className="image">
+            <img src={logo} className="App-logo" alt="Michelle playing guitar" />  
+          </div>
+
+          {/* Socials */}
           <div className="social-box">
-          <SocialIcon className="social" bgColor="white" url="https://www.youtube.com/user/MichelleKMoyer"/>
-          <SocialIcon className="social" bgColor="white" url="https://instagram.com/michellemoyermusic?utm_medium=copy_link"/>
-          <SocialIcon className="social" bgColor="white" url="https://open.spotify.com/artist/4Y8GKCTKBhbwcl0nrSizwq?si=nAqW8Q0uTZ-ilQDgcadYTg"/>
-          <SocialIcon className="social" bgColor="white" network="itunes" url="https://music.apple.com/us/artist/michelle-moyer/389392280"/>
+            <SocialIcon className="social" bgColor="white" url="https://www.youtube.com/user/MichelleKMoyer"/>
+            <SocialIcon className="social" bgColor="white" url="https://instagram.com/michellemoyermusic?utm_medium=copy_link"/>
+            <SocialIcon className="social" bgColor="white" url="https://open.spotify.com/artist/4Y8GKCTKBhbwcl0nrSizwq?si=nAqW8Q0uTZ-ilQDgcadYTg"/>
+            <SocialIcon className="social" bgColor="white" network="itunes" url="https://music.apple.com/us/artist/michelle-moyer/389392280"/>
         </div>
         </div>
       </div>
